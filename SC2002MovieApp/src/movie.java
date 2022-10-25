dc774e2572c226364c053385b317a17caf93d3b0
tehnii <yannarojc@outlook.com>
2022-10-25 15:06:14 +0800

    Movie Class Skeleton

---
+++ SC2002MovieApp/src/movie.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+
+public class movie {
+    private String movieId;
+    private String title;
+    private boolean blockBuster;
+    private String typeOfMovie[];
+    private int status;
+    private String director;
+    private String cast[];
+    private float overallRating;
+    private String pastReview[];
+
+
+
+}
