692b68e5e299f229e1b9567866716b402cc30fc5
tehnii <yannarojc@outlook.com>
2022-10-25 15:25:26 +0800

    Entity folder

---
+++ SC2002MovieApp/src/Entity/movie.java
@@ -0,0 +1,22 @@
+package Entity;
+import java.lang.reflect.Array;
+
+public class movie {
+    private String movieId;
+    private String title;
+    private boolean blockBuster;
+    private String typeOfMovie[];
+    private int status;
+    private String director;
+    private String cast[];
+    private float overallRating;
+    private String pastReview[];
+
+    public String getTitle() {
+        return title;
+      }
+
+
+
+
+}
