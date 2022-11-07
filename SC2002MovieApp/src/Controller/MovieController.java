8cb83f8bd06539b63d5a1fe526eefcdebcf06493
Fabian Tay <104303126+fabbiotaye@users.noreply.github.com>
2022-11-07 17:16:28 +0800

    added methods to update movie details (incomplete)

--- SC2002MovieApp/src/Controller/MovieController.java
+++ SC2002MovieApp/src/Controller/MovieController.java
@@ -44,6 +44,72 @@ public class MovieController {
         }
     }
 
+    // update movie status
+    public static void updateStatus(ArrayList<movie> MovieList, int movieId, int inputValue) {
+        boolean success = false;
+        for (movie Movie : MovieList) {
+            if (Movie.getMovieID() == movieId) {
+                if (inputValue < 0 || inputValue > 2) {
+                    success = true;
+                    System.out.println("Invalid input");
+                } else {
+                    Movie.setStatus(inputValue);
+                    success = true;
+                    System.out.println("Movie status successfully updated");
+                }
+                break;
+            }
+        }
+        if (!success) {
+            System.out.println("Movie ID not found");
+        }
+    }
+
+    // set blockbuster status
+    public static void updateBlockBuster(ArrayList<movie> MovieList, int movieId, int inputValue) {
+        boolean success = false;
+        for (movie Movie : MovieList) {
+            if (Movie.getMovieID() == movieId) {
+                success = true;
+                switch (inputValue) {
+                    case 0:
+                        Movie.setNonBlockBuster();
+                        System.out.println(" Movie successfully set to non-blockbuster");
+                        break;
+                    case 1:
+                        Movie.setBlockBuster();
+                        System.out.println("Movie successfully set to blockbuster");
+                        break;
+                    default:
+                        System.out.println("Invalid input");
+                        break;
+                }
+                break;
+            }
+        }
+        if (!success) {
+            System.out.println("Movie ID not found");
+        }
+    }
+
+    // update individual movie details using String variables
+    public static void updateIndivMovieDetail(ArrayList<movie> MovieList, int movieId, String inputString, int choice) {
+        boolean success = false;
+        for (movie Movie : MovieList) {
+            if (Movie.getMovieID() == movieId) {
+                success = true;
+                switch (choice) {
+                    // to fill up later
+                }
+                break;
+            }
+        }
+        if (!success) {
+            System.out.println("Movie ID not found");
+        }
+
+    }
+
     // print individual movie details
     public static void printSingleMovie(movie Movie) {
         System.out.println("Movie ID: " + Movie.getMovieID());
