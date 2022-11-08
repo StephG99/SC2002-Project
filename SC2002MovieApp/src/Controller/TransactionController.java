3feb99a1cc08b22e067d24acfa1800356b05cb87
tehnii <yannarojc@outlook.com>
2022-11-08 17:51:28 +0800

    Some Controllers

---
+++ SC2002MovieApp/src/Controller/TransactionController.java
@@ -0,0 +1,6 @@
+package Controller;
+
+//booking a movie should be done here
+public class TransactionController {
+    
+}
--- SC2002MovieApp/src/Controller/UserController.java
+++ SC2002MovieApp/src/Controller/UserController.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 
 
-//Login Controller settles login for all User. we do not store payment information for customer
+//User Controller settles everything for all User. we do not store payment information for customer
 public class UserController {
     public static User loginUser(String email,String password){
         ArrayList<User> tempArray = DatabaseController.getAllUser();
@@ -21,10 +21,13 @@ public class UserController {
         //no User found Proceed to return null object.
         return null;
     }
+    //Register User method
     public static User registerUser(String name,String email,String password) throws IOException{
+        // If user exist returns a null object to View Page (View Page should be able to handle null objects;)
         if(DatabaseController.getUser(email) != null){
             return null;
         }
+        //else if no user exist we shall call the call database to insert the user in. Note that we do the creation of user object here.
         return DatabaseController.registerUser(new User(name,email,password,false));
 
     }
