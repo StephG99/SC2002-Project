3ac04b05c870831dc099517f6243d7873e04a563
Fabian Tay <104303126+fabbiotaye@users.noreply.github.com>
2022-11-09 19:51:24 +0800

    change a bit

--- SC2002MovieApp/src/Entity/Cineplex.java
+++ SC2002MovieApp/src/Entity/Cineplex.java
@@ -1,32 +1,40 @@
 package Entity;
 
+import java.util.*;
+
 public class Cineplex {
     private int cineplexId;
-    private Cinema cinemas[];
+    private ArrayList<Cinema> cinemas;
     private movieClass classOffered[];
 
-    public Cineplex(int cineplexId, Cinema[] cinemas, movieClass[] classOffered) {
+    public Cineplex(int cineplexId, ArrayList<Cinema> cinemas, movieClass[] classOffered) {
         this.cineplexId = cineplexId;
         this.cinemas = cinemas;
         this.classOffered = classOffered;
     }
+
     public int getCineplexId() {
         return cineplexId;
     }
+
     public void setCineplexId(int cineplexId) {
         this.cineplexId = cineplexId;
     }
-    public Cinema[] getCinemas() {
+
+    public ArrayList<Cinema> getCinemas() {
         return cinemas;
     }
-    public void setCinemas(Cinema[] cinemas) {
+
+    public void setCinemas(ArrayList<Cinema> cinemas) {
         this.cinemas = cinemas;
     }
+
     public movieClass[] getClassOffered() {
         return classOffered;
     }
+
     public void setClassOffered(movieClass[] classOffered) {
         this.classOffered = classOffered;
     }
-    
+
 }
