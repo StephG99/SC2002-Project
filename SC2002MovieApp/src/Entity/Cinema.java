94cb67c4e75aa1f60e3db7fa61c685ef3734149c
Fabian Tay <104303126+fabbiotaye@users.noreply.github.com>
2022-11-09 19:28:38 +0800

    added setter methods

--- SC2002MovieApp/src/Entity/Cinema.java
+++ SC2002MovieApp/src/Entity/Cinema.java
@@ -7,7 +7,7 @@ public class Cinema {
     private movieClass cinemaClass;
     private Seat[][] seats = new Seat[10][10];
 
-    public Cinema(int cinemaID,movieClass cinemaClass) {
+    public Cinema(int cinemaID, movieClass cinemaClass) {
         this.cinemaID = cinemaID;
         this.cinemaClass = cinemaClass;
         for (int i = 0; i < 10; i++) {
@@ -22,16 +22,28 @@ public class Cinema {
         return cinemaID;
     }
 
-    public int getSeatsAvailable(){
+    public void setCinemaID(int cinemaID) {
+        this.cinemaID = cinemaID;
+    }
+
+    public int getSeatsAvailable() {
         int counter = 0;
-        for(int i = 0; i < 10;i++){
-            for(int j= 0;j<10;j++){
-                if(!seats[i][j].isOccupied()){
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (!seats[i][j].isOccupied()) {
                     counter++;
                 }
             }
         }
         return counter;
+    }
+
+    public movieClass getCinemaClass() {
+        return cinemaClass;
+    }
+
+    public void setCinemaClass(movieClass cinemaClass) {
+        this.cinemaClass = cinemaClass;
     }
 
 }
