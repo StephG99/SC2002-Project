16aff1470fddb6a868abc6b5e73ccb1e96b430b2
Fabian Tay <104303126+fabbiotaye@users.noreply.github.com>
2022-11-10 17:11:46 +0800

    attempt at reading new Date and time format, plus several new database control methods

---
+++ SC2002MovieApp/src/View/BookMoviePage.java
@@ -0,0 +1,26 @@
+package View;
+
+import Helper.Helper;
+import Entity.*;
+import Controller.*;
+
+import java.util.*;
+
+public class BookMoviePage {
+    // display header
+    public static void displayBookingHeader(){
+        Helper.line(80, "=");
+		System.out.println("Book a Movie Ticket");
+		Helper.line(80, "=");
+    }
+
+
+    // show available timings for selected movie and cinema
+    public static void printShowtimes(){
+
+    }
+
+
+    // show available seats for selected showtime
+
+}
