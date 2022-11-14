f14732bd59739315a2df5e7d8fa9d631a615f07b
tehnii <yannarojc@outlook.com>
2022-11-14 17:15:08 +0800

    setting java doc done

--- SC2002MovieApp/src/Entity/Settings.java
+++ SC2002MovieApp/src/Entity/Settings.java
@@ -12,7 +12,15 @@ public class Settings {
     private double weekendRates;
     
     
-
+    /**
+     * 
+     * @param regularPrice
+     * @param seniorPrice
+     * @param studentPrice
+     * @param publicHolidays
+     * @param holidayRates
+     * @param weekendRates
+     */
     public Settings(double regularPrice, double seniorPrice, double studentPrice, ArrayList<Date> publicHolidays,
             double holidayRates,double weekendRates) {
         this.regularPrice = regularPrice;
@@ -22,46 +30,89 @@ public class Settings {
         this.holidayRates = holidayRates;
         this.weekendRates = weekendRates;
     }
+    /**
+     * 
+     * @return a double of the regular price
+     */
     public double getRegularRates() {
         return regularPrice;
     }
-
+     
+    /**
+     * 
+     * @param regularPrice set new regular price
+     */
     public void setRegular(double regularPrice) {
         this.regularPrice = regularPrice;
     }
+    /**
+     * 
+     * @return a double of the senior price
+     */
     public double getSeniorRates() {
         return seniorPrice;
     }
-
+    /**
+     * 
+     * @param seniorRates set new senior rates
+     */
     public void setSeniorRates(double seniorRates) {
         this.seniorPrice = seniorRates;
     }
+    /**
+     * 
+     * @return a double of the Student price
+     */
     public double getStudentRates() {
         return studentPrice;
     }
-
+    /**
+     * 
+     * @param studentPrice set new Student rates
+     */
     public void setStudentRates(double studentPrice) {
         this.studentPrice = studentPrice;
     }
+    /**
+     * 
+     * @return a array of public holiday rate
+     */
     public ArrayList<Date> getPublicHolidays() {
         return PublicHolidays;
     }
 
+    /**
+     * 
+     * @param publicHolidays set new public holiday dates for setting
+     */
     public void setPublicHolidays(ArrayList<Date> publicHolidays) {
         PublicHolidays = publicHolidays;
     }
-
+    /**
+     * 
+     * @return holiday rates
+     */
     public double getHolidayRates() {
         return holidayRates;
     }
-
+    /**
+     * 
+     * @param holidayRates set new Holiday rates
+     */
     public void setHolidayRates(double holidayRates) {
         this.holidayRates = holidayRates;
     }
+    /**
+     * 
+     * @return weekend price
+     */
     public double getWeekendRates() {
         return regularPrice;
     }
-
+    /**
+     * 
+     * @param weekendRates weekend prices
+     */
     public void setWeekendRates(double weekendRates) {
         this.weekendRates = weekendRates;
     }
